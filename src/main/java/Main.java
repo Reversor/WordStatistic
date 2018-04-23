d80299fdf04451227e6d7a11bdca0459e849439e
reversor <revers914@gmail.com>
2018-04-23 20:28:28 +0400

    how delete this evil: –

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
+import java.io.PrintStream;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
@@ -21,28 +25,37 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("Word count").setMaster("local[*]");
 //        String[] garbage = new String[]{"\n", "", "и", "в"};
         try (JavaSparkContext sc = new JavaSparkContext(conf)) {
-            String pathTolstoy = "src/main/resources/Tolstoy/*.txt";
-            String pathMarks = "src/main/resources/Marks/*.txt";
-            String pathDuma = "src/main/resources/Duma/*.txt";
-            wordRatingRDD(sc, pathTolstoy).leftOuterJoin(wordRatingRDD(sc, pathMarks)).sortByKey(false).take(20)
-                    .forEach(System.out::println);
+            JavaPairRDD<String, Long> ratingTolstoy =
+                    wordRatingRDD(sc, "src/main/resources/Tolstoy/*.txt");
+            JavaPairRDD<String, Long> ratingMarks =
+                    wordRatingRDD(sc, "src/main/resources/Marks/*.txt");
+            JavaPairRDD<String, Long> ratingDuma =
+                    wordRatingRDD(sc, "src/main/resources/Duma/*.txt");
+            List<Tuple2<Long, Iterable<String>>> result1 = ratingTolstoy.intersection(ratingMarks)
+                    .map(Tuple2::swap).mapToPair(t -> t).groupByKey().sortByKey(false).takeAsync(20).get();
+            System.out.println("Топ 20 слов из \"Война и мир\" Толстого в \"Капитал\" Маркса:");
+            result1.forEach(System.out::println);
+            List<Tuple2<Long, Iterable<String>>> result2 = ratingTolstoy
+                    .subtractByKey(ratingMarks).mapToPair(Tuple2::swap).groupByKey()
+                    .sortByKey(false).takeAsync(20).get();
+            System.out.println("Топ 20 слов из \"Война и мир\" Толстого которых нет в \"Капитал\" Маркса:");
+            result2.forEach(System.out::println);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
-    private static JavaPairRDD<Long, Iterable<String>> wordRatingRDD(JavaSparkContext sc, String path) {
-        JavaRDD<String> garbage = sc.textFile("src/main/resources/garbage.txt");
-
+    private static JavaPairRDD<String, Long> wordRatingRDD(JavaSparkContext sc, String path) {
         return sc.textFile(path)
                 .distinct()
                 .flatMap(s -> Iterators.forArray(s.split("\\p{Space}")))
-                .map(String::toLowerCase)
-                .map(s -> s.replaceAll("\\p{Punct}", ""))
-                .subtract(garbage)
+                .map(StringUtils::capitalize)
+                .map(s -> s.replaceAll("\\p{Blank}|\\p{Graph}", ""))
+                .subtract(sc.textFile("src/main/resources/garbage.txt"))
                 .filter(s -> s.length() > 1)
                 .mapToPair(s -> new Tuple2<>(s, 1L))
                 .reduceByKey(Long::sum)
-                .map(Tuple2::swap)
                 .mapToPair(t -> t)
-                .groupByKey();
+                .persist(StorageLevel.MEMORY_AND_DISK());
     }
 }
